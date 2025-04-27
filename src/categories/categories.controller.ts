ecee004a537c7fcea251175f16a5caea2b4ec7e7
Sardor Abdullaev <abdullaevss94@mail.ru>
2025-04-28 02:10:44 +0500

    add swagger

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,17 +9,24 @@ import {
     UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
+import { CreateCategoryDto, CreateCategoryResponseDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOperation, ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Category } from './category.entity';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('categories')
 export class CategoriesController {
     constructor(private readonly categoriesService: CategoriesService) { }
 
+    @ApiOperation({ summary: 'Create a new category(only Admin)' })
+    @ApiCreatedResponse({ description: 'Category created', type: CreateCategoryResponseDto }) // @ApiResponse({ status: 201, description: 'Category created', type: CreateCategoryResponseDto })
+    @ApiBadRequestResponse({ description: 'There must be a name at least' })
+    @ApiNotFoundResponse({ description: 'Parent category not found' })
+    @ApiUnauthorizedResponse({description:"Unauthorized"})
     @Post()
     @Roles('admin')
     create(@Body() createCategoryDto: CreateCategoryDto) {
@@ -27,23 +34,33 @@ export class CategoriesController {
     }
 
     @Get()
+    @ApiOperation({ summary: 'Get all categories' })
+    @ApiResponse({ status: 200, description: 'List of categories.', type: [Category] })
     findAll() {
         return this.categoriesService.findAll();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get a category by ID' })
+    @ApiResponse({ status: 200, description: 'The found category.', type: Category })
     findOne(@Param('id') id: string) {
         return this.categoriesService.findOne(id);
     }
 
     @Patch(':id')
     @Roles('admin')
+    @ApiOperation({ summary: 'Update a category (only Admin)' })
+    @ApiResponse({ status: 200, description: 'Category updated successfully.', type: Category })
+    @ApiResponse({ status: 404, description: 'Category not found.' })
     update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
         return this.categoriesService.update(id, updateCategoryDto);
     }
 
     @Delete(':id')
     @Roles('admin')
+    @ApiOperation({ summary: 'Delete a category (only Admin)' })
+    @ApiResponse({ status: 200, description: 'Category deleted successfully.' })
+    @ApiResponse({ status: 404, description: 'Category not found.' })
     remove(@Param('id') id: string) {
         return this.categoriesService.remove(id);
     }
--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,6 +1,32 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
 export class CreateCategoryDto {
+    @ApiProperty({ example: 'Electronics', description: 'The name of the category' })
     name: string;
+
+    @ApiPropertyOptional({ example: 'Devices and gadgets', description: 'A short description of the category', required: false })
     description?: string;
+
+    @ApiPropertyOptional({ example: 'electronics.png', description: 'Image URL for the category' })
     image?: string;
+
+    @ApiProperty({ example: null, description: 'Parent category ID if this is a sub-category', required: false })
+    parentId?: string;
+}
+
+export class CreateCategoryResponseDto {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty({ example: 'Electronics', description: 'The name of the category' })
+    name: string;
+
+    @ApiPropertyOptional({ example: 'Devices and gadgets', description: 'A short description of the category' })
+    description?: string;
+
+    @ApiPropertyOptional({ example: 'electronics.png', description: 'Image URL for the category' })
+    image?: string;
+
+    @ApiPropertyOptional({ example: '2852619e-d363-4c08-9321-f48c5b424dd2', description: 'ID of the parent category if it exists' })
     parentId?: string;
 }
